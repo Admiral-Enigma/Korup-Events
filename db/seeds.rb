60172a38d98ea97792f4e53a168ffe9a28363f2b
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2017-03-22 18:43:06 +0100

    Added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+eventList = [
+  ["Gameraften på Korup skole", "6/9/17", 0, true],
+  ["Fest på Korup skole", "9/9/17", 50, true],
+  ["Blåfest", "12/11/17", 30, true],
+  ["Olsenbanden film aften", "6/12/17", 20, true]
+]
+
+eventList.each do |title, date, price, accepted|
+  Event.create(title: title, date: date, price: price, accepted: accepted)
+end
