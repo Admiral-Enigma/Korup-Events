8709fd16dbb6d7d034b557c4b1f7d55bf1f29b3f
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2017-03-29 18:06:25 +0200

    Added contact model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
+  get '/events/new', to: 'event#new'
+  post '/events', to: 'event#create'
   get '/about', to: 'events#about'
   get '/events/:id', to: 'events#show'
+
+  get '/admin', to: 'admin#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322172823) do
+ActiveRecord::Schema.define(version: 20170329160431) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
