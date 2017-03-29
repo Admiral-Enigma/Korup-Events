54adb5250ef5522d3cdf5c8d4e7df2325ee59300
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2017-03-29 18:44:24 +0200

    Now you can save events!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,24 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(get_params_event)
+    @event.accepted = true
+    if @event.save
+      redirect_to '/congrats'
+    end
+  end
+
+  def congrats
+
+  end
+
   def show
     @event = Event.find(params[:id])
   end
+
+  private
+    def get_params_event
+      params.require(:event).permit(:title, :date, :price, :contact_name, :contact_email, :contact_number, :more_info_heading, :more_info_body_text)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
-  get '/events/new', to: 'event#new'
-  post '/events', to: 'event#create'
+  get '/congrats', to: 'events#congrats'
+  get '/events/new', to: 'events#new'
+  post '/events', to: 'events#create'
   get '/about', to: 'events#about'
   get '/events/:id', to: 'events#show'
 
